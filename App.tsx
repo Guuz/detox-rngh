2c018184b2dd441f66dbe1ca9e1f4dbd407e5ab9
Guus Goossens <guus.goossens@gmail.com>
2020-07-24 17:05:46 +0200

    Tests for RNGH.

--- App.tsx
+++ App.tsx
@@ -8,6 +8,8 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
+import {TouchableHighlight as RNGHTouchableHighlight, RectButton} from 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
@@ -17,29 +19,48 @@ import {
   Text,
   StatusBar,
   TouchableHighlight,
+  Button,
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
 declare const global: {HermesInternal: null | {}};
 
 const App = () => {
+  const [taps, setTaps] = React.useState<string[]>([])
+
+  function addTap(tapName: string) {
+    setTaps((taps) => [...taps, tapName])
+  }
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
         <ScrollView style={styles.scrollView}>
           <SafeAreaView>
             <View style={styles.buttons}>
-              <TouchableHighlight onPress={() => console.log('React Native TouchableHighlight pressed')}>
+
+              <TouchableHighlight onPress={() => addTap('React Native TouchableHighlight pressed')}>
                 <Text>React Native</Text>
               </TouchableHighlight>
-              </View>
+
+              <Button title="React Native Button" onPress={() => addTap('React Native Button pressed')} />
+
+              <RNGHTouchableHighlight onPress={() => addTap('React Native RNGHTouchableHighlight pressed')}>
+                <Text>React Native Gesture Handler</Text>
+              </RNGHTouchableHighlight>
+
+              <RectButton onPress={() => addTap('RectButton pressed')}>
+                <Text>RectButton</Text>
+              </RectButton>
+
+            </View>
+            <View style={styles.buttons}>
+              {taps.map((tap, i) => <Text key={i}>{tap}</Text>)}
+            </View>
+
           </SafeAreaView>
         </ScrollView>
     </>
--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -7,4 +7,19 @@ describe('Example', () => {
     await expect(element(by.text('React Native'))).toBeVisible();
     await element(by.text('React Native')).tap();
   });
+
+  it('should press a React Native Button by text', async () => {
+    await expect(element(by.text('React Native Button'))).toBeVisible();
+    await element(by.text('React Native Button')).tap();
+  });
+
+  it('should press a React Native Gesture Handler TouchableHighlight by text', async () => {
+    await expect(element(by.text('React Native Gesture Handler'))).toBeVisible();
+    await element(by.text('React Native Gesture Handler')).tap();
+  });
+
+  it('should press a RectButton by text', async () => {
+    await expect(element(by.text('RectButton'))).toBeVisible();
+    await element(by.text('RectButton')).tap();
+  });
 });
